7928b59a71a625227c9bcb0ad906b8559bb76fc7
Hanna Anvar <63791597+Hanna025@users.noreply.github.com>
2022-10-22 12:27:08 +0530

    Update isPrime.c

--- Simple C Programs/isPrime.c
+++ Simple C Programs/isPrime.c
@@ -6,30 +6,30 @@ int prime(int);
 #include"PrimeHead.h"
 int prime(int n)
 {
-int j=0,i;
-for(i=1;i<=n;i++)
-{
-if(n%i==0)
-j++;
-}
-if(j==2)
-return 0;
-else
-return 1;
+        int j=0,i;
+        for(i=1;i<=n;i++)
+        {
+                if(n%i==0)
+                        j++;
+        }
+        if(j==2)
+                return 0;
+        else
+                return 1;
 }
 
 //PrimeApp.c
 #include"PrimeHead.h"
 int main()
 {
-int res,a;
-printf("Enter the value for a\n");
-scanf("%d",&a);
-res=prime(a);
-if(res==0)
-printf(" Prime Number");
-else
-printf(" Not a Prime Number");
+        int res,a;
+        printf("Enter the value for a\n");
+        scanf("%d",&a);
+        res=prime(a);
+        if(res==0)
+                printf(" Prime Number");
+        else
+                printf(" Not a Prime Number");
 }
 
 //makefile
