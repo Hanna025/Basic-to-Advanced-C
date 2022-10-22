2ca583418fec87a1d31db5d21a0e1149a23056c4
Hanna Anvar <63791597+Hanna025@users.noreply.github.com>
2022-10-22 12:24:51 +0530

    Update armstrongNum.c

--- Simple C Programs/armstrongNum.c
+++ Simple C Programs/armstrongNum.c
@@ -7,37 +7,37 @@ int armstrong(int);
 #include"ArmstrongHead.h"
 int armstrong(int a)
 {
-int  m,n,c=0,rem=0,res=0;
-m=n=a;
-while(m!=0)
-{
-m=m/10;
-c=c+1;
-}
-while(n!=0)
-{
-rem=n%10;
-res=res+pow(rem,c);
-n=n/10;
-}
-if(res==a)
- return 1;
-else
-return 0;
+	int  m,n,c=0,rem=0,res=0;
+	m=n=a;
+	while(m!=0)
+	{
+		m=m/10;
+		c=c+1;
+	}
+	while(n!=0)
+	{
+		rem=n%10;
+		res=res+pow(rem,c);
+		n=n/10;
+	}
+	if(res==a)
+ 		return 1;
+	else
+		return 0;
 }
 
 //ArmstrongApp.c
 #include"ArmstrongHead.h"
 int main()
 {
-int x,result;
-printf("Enter the value for a-\n");
-scanf("%d",&x);
-result=armstrong(x);
-if(result==1)
-printf(" %d Armstrong Number",x);
-else
-printf(" %d Not an Armstrong Number",x);
+	int x,result;
+	printf("Enter the value for a-\n");
+	scanf("%d",&x);
+	result=armstrong(x);
+	if(result==1)
+		printf(" %d Armstrong Number",x);
+	else
+		printf(" %d Not an Armstrong Number",x);
 }
 
 //makefile
