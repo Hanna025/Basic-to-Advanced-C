7db180b745029521d28d6f75c555f98f2b2e7391
Hanna Anvar <63791597+Hanna025@users.noreply.github.com>
2022-10-22 11:57:01 +0530

    Create Vowel.c

---
+++ Simple C Programs/Vowel.c
@@ -0,0 +1,46 @@
+//VowelHead.h
+#include<stdio.h>
+int comp(char);
+
+//VowelImp.c
+#include"VowelHead.h"
+int comp(char n)
+{
+if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u')
+return 1;
+else
+return 2;
+}
+
+//VowelApp.c
+#include"VowelHead.h"
+int main()
+{
+char b;
+int op;
+printf("Enter character-\n");
+scanf("%c",&b);
+op=comp(b);
+switch(op)
+{
+case 1:
+printf("Is a vowel");
+break;
+
+case 2:
+printf("Not a vowel");
+break;
+
+default:
+printf("Invalid input");
+break;
+}
+}
+
+//makefile
+Vowel.exe:VowelImp.o VowelApp.o
+        cc -o Vowel.exe VowelImp.o VowelApp.o
+VowelImp.o:VowelImp.c
+        cc -c VowelImp.c
+VowelApp.o:VowelApp.c
+        cc -c VowelApp.c
