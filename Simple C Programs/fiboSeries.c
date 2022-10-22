59207804d5df7c999de28d8768aca22ec0051e3e
Hanna Anvar <63791597+Hanna025@users.noreply.github.com>
2022-10-22 12:26:03 +0530

    Update fiboSeries.c

--- Simple C Programs/fiboSeries.c
+++ Simple C Programs/fiboSeries.c
@@ -6,24 +6,24 @@ int fibo(int);
 #include"FibonacciHead.h"
 int fibo(int b)
 {
-int i,m=0,n=1,next_term;
-for(i=1;i<=b;i++)
-{
-printf("%d\t",m);
-next_term=m+n;
-m=n;
-n=next_term;
-}
+        int i,m=0,n=1,next_term;
+        for(i=1;i<=b;i++)
+        {
+                printf("%d\t",m);
+                next_term=m+n;
+                m=n;
+                n=next_term;
+        }
 }
 
 //FibonacciApp.c
 #include"FibonacciHead.h"
 int main()
 {
-int a ,res;
-printf("Enter the value for a-\n");
-scanf("%d",&a);
-res=fibo(a);
+        int a ,res;
+        printf("Enter the value for a-\n");
+        scanf("%d",&a);
+        res=fibo(a);
 }
 
 //makefile
