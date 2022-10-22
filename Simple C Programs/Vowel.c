34a3dd3ad0255f5c1ac04e556db213b007a0f477
Hanna Anvar <63791597+Hanna025@users.noreply.github.com>
2022-10-22 12:23:35 +0530

    Update Vowel.c

--- Simple C Programs/Vowel.c
+++ Simple C Programs/Vowel.c
@@ -6,35 +6,35 @@ int comp(char);
 #include"VowelHead.h"
 int comp(char n)
 {
-if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u')
-return 1;
-else
-return 2;
+        if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u')
+                return 1;
+        else
+                return 2;
 }
 
 //VowelApp.c
 #include"VowelHead.h"
 int main()
 {
-char b;
-int op;
-printf("Enter character-\n");
-scanf("%c",&b);
-op=comp(b);
-switch(op)
-{
-case 1:
-printf("Is a vowel");
-break;
+        char b;
+        int op;
+        printf("Enter character-\n");
+        scanf("%c",&b);
+        op=comp(b);
+        switch(op)
+        {
+                case 1:
+                        printf("Is a vowel");
+                        break;
 
-case 2:
-printf("Not a vowel");
-break;
+                case 2:
+                        printf("Not a vowel");
+                        break;
 
-default:
-printf("Invalid input");
-break;
-}
+                default:
+                        printf("Invalid input");
+                        break;
+        }
 }
 
 //makefile
