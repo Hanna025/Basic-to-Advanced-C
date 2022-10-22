1df8ec4af587788fdab90ce69dbe955c930350d3
Hanna Anvar <63791597+Hanna025@users.noreply.github.com>
2022-10-22 11:59:56 +0530

    Create SumofNnumbers.c

---
+++ Simple C Programs/SumofNnumbers.c
@@ -0,0 +1,34 @@
+//SumHead.h
+#include<stdio.h>
+int sum();
+
+//SumImp.c
+#include"SumHead.h"
+int sum(int n)
+{
+int i=1,sum=0;
+do
+{
+sum=sum+i;
+i=i+1;
+}
+while (i<=n);
+return sum;
+}
+//SumApp.c
+#include"SumHead.h"
+int main()
+{
+int res,a;
+printf("Enter the value for a\n");
+scanf("%d",&a);
+res=sum(a);
+printf("The sum is %d",res);
+}
+//makefile
+Sum.exe:SumImp.o SumApp.o
+        cc -o Sum.exe SumImp.o SumApp.o
+SumImp.o:SumImp.c
+        cc -c SumImp.c
+SumApp.o:SumApp.c
+        cc -c SumApp.c
