d50042d92bf6df44048d1e5f4284d27cf6e8915f
Hanna Anvar <63791597+Hanna025@users.noreply.github.com>
2022-10-22 12:20:44 +0530

    Update StudentGrading.c

--- Simple C Programs/StudentGrading.c
+++ Simple C Programs/StudentGrading.c
@@ -6,26 +6,23 @@ int grade(int);
 #include"GradeHead.h"
 int grade (int m)
 {
-if(m>=90)
-return 1;
+        if(m>=90)
+                return 1;
 
-else
-if(m>=80&&m<90)
-return 2;
+        elseif(m>=80&&m<90)
+                return 2;
 
-else
-if(m>=70&&m<80)
-return 3;
+        elseif(m>=70&&m<80)
+                return 3;
 
-else
-if(m>=60&&m<70)
-return 4;
+        elseif(m>=60&&m<70)
+                return 4;
 
-if(m>=50&&m<60)
-return 5;
+        elseif(m>=50&&m<60)
+                return 5;
 
-else
-return 6;
+        else
+                return 6;
 }
 
 
@@ -33,31 +30,27 @@ return 6;
 #include"GradeHead.h"
 int main()
 {
-int a , i;
-printf("Enter your marks:\n");
-scanf("%d",&a);
-i=grade(a);
-if(i==1)
-printf("Grade is O");
+        int a , i;
+        printf("Enter your marks:\n");
+        scanf("%d",&a);
+        i=grade(a);
+        if(i==1)
+                printf("Grade is O");
 
-else
-if(i==2)
-printf("Grade is A+");
+        elseif(i==2)
+                printf("Grade is A+");
 
-else
-if(i==3)
-printf("Grade is A");
+        elseif(i==3)
+                printf("Grade is A");
 
-else
-if(i==4)
-printf("Grade is B+");
+        elseif(i==4)
+                printf("Grade is B+");
 
-else
-if(i==5)
-printf("grade is B");
+        elseif(i==5)
+                printf("grade is B");
 
-else
-printf("fail");
+        else
+                printf("fail");
 }
 //makefile
 Grade.exe: GradeImp.o GradeApp.o
